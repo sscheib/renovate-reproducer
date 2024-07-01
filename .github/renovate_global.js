415f036277f8382470be58722efedfe38672e9c7
Steffen Scheib <steffen@scheib.me>
2024-07-01 15:32:46 +0200

    removing irrelevant data

--- .github/renovate_global.js
+++ .github/renovate_global.js
@@ -1,6 +1,5 @@
 module.exports = {
   secrets: {
-    AUTOMATION_HUB_AUTH_TOKEN: process.env.RENOVATE_AUTOMATION_HUB_AUTH_TOKEN,
     CRC_USERNAME: process.env.RENOVATE_CRC_USERNAME,
     CRC_PASSWORD: process.env.RENOVATE_CRC_PASSWORD,
   },
@@ -9,18 +8,6 @@ module.exports = {
   ],
   gitAuthor: 'Renovate <renovate@scheib.me>',
   hostRules: [
-    {
-      matchHost: 'https://console.redhat.com/api/automation-hub/content/published/',
-      headers: {
-        Authorization: 'Bearer {{ secrets.AUTOMATION_HUB_AUTH_TOKEN }}',
-      },
-    },
-    {
-      matchHost: 'https://console.redhat.com/api/automation-hub/content/validated/',
-      headers: {
-        Authorization: 'Bearer {{ secrets.AUTOMATION_HUB_AUTH_TOKEN }}',
-      },
-    },
     {
       matchHost: 'registry.redhat.io',
       username: '{{ secrets.CRC_USERNAME }}',
