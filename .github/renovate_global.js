a961eeb9e3a4cbbc29ba8ac8127d6cf6a6019e38
Steffen Scheib <steffen@scheib.me>
2024-09-01 15:10:14 +0200

    ci: Troubleshooting EEs
    Signed-off-by: Steffen Scheib <steffen@scheib.me>

--- .github/renovate_global.js
+++ .github/renovate_global.js
@@ -1,7 +1,9 @@
 module.exports = {
   secrets: {
+    AUTOMATION_HUB_AUTH_TOKEN: process.env.RENOVATE_AUTOMATION_HUB_AUTH_TOKEN,
     CRC_USERNAME: process.env.RENOVATE_CRC_USERNAME,
     CRC_PASSWORD: process.env.RENOVATE_CRC_PASSWORD,
+    GHCR_TOKEN: process.env.RENOVATE_GHCR_TOKEN,
   },
   allowedHeaders: [
     'Authorization',
@@ -9,9 +11,36 @@ module.exports = {
   gitAuthor: 'Renovate <renovate@scheib.me>',
   hostRules: [
     {
+      matchHost: 'https://console.redhat.com/api/automation-hub/content/published/',
+      headers: {
+        Authorization: 'Bearer {{ secrets.AUTOMATION_HUB_AUTH_TOKEN }}',
+      },
+    },
+    {
+      matchHost: 'https://console.redhat.com/api/automation-hub/content/validated/',
+      headers: {
+        Authorization: 'Bearer {{ secrets.AUTOMATION_HUB_AUTH_TOKEN }}',
+      },
+    },
+    {
+      matchHost: 'https://registry.access.redhat.com',
+      username: '{{ secrets.CRC_USERNAME }}',
+      password: '{{ secrets.CRC_PASSWORD }}',
+    },
+    {
       matchHost: 'registry.redhat.io',
       username: '{{ secrets.CRC_USERNAME }}',
       password: '{{ secrets.CRC_PASSWORD }}',
+    },
+    {
+      matchHost: 'ghcr.io',
+      username: '{{ process.env.RENOVATE_GHCR_USERNAME }}',
+      password: '{{ secrets.GHCR_TOKEN }}',
+    },
+    {
+      matchHost: "registry.npmjs.org",
+      timeout: 240000,
+      enableHttp2: false
     },
   ],
 };
